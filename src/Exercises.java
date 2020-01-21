571852272b52a66c64c2c27ef9e11ed8128b394d
Sambhavi Dwivedi <sambhavi@Sambhavis-MBP.hsd1.nj.comcast.net>
2020-01-20 21:41:41 -0500

    increasing done

--- src/Exercises.java
+++ src/Exercises.java
@@ -104,6 +104,15 @@ public class Exercises {
 	// default return value to ensure compilation
 
 	public boolean increasing(int[] numbers) {
+		if (numbers == null || numbers.length < 3) {
+		return false;
+}
+
+for (int i = 1; i < numbers.length - 1; i++) {
+		if ((numbers[i - 1] == (numbers[i] - 1)) && (numbers[i] == (numbers[i + 1] - 1))) {
+				return true;
+		}
+}
 		// write your code here
 
 		return false;	// default return value to ensure compilation
